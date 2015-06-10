5ff853e23cfca48613a7030df887aaa83fbf9ab5
YanHua <yanhua_92@hotmail.com>
2015-06-10 19:34:44 +0800

    Add missing comma.

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ module.exports = {
   app: {
     title: 'Codeaux - Development Environment',
     description: 'Codeaux development stage'
-  }
+  },
 
   db: 'mongodb://localhost/codeaux-dev',
   port: process.env.PORT || 3000
