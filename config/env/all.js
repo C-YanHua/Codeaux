71b7cd0fe326b517322900a3dca88fc1beeae6cb
YanHua <yanhua_92@hotmail.com>
2015-06-10 19:17:05 +0800

    Update application default configurations.

--- config/env/all.js
+++ config/env/all.js
@@ -1,45 +1,90 @@
 'use strict';
 
+// Default configurations unless specify in another environment JS file.
 module.exports = {
-	app: {
-		title: 'Codeaux',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'MongoDB, Express, AngularJS, Node.js'
-	},
-	port: process.env.PORT || 3000,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-			],
-			js: [
-				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js',
-				'public/lib/angular-cookies/angular-cookies.js',
-				'public/lib/angular-animate/angular-animate.js',
-				'public/lib/angular-touch/angular-touch.js',
-				'public/lib/angular-sanitize/angular-sanitize.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
-			]
-		},
-		css: [
-			'public/modules/**/css/*.css'
-		],
-		js: [
-			'public/config.js',
-			'public/application.js',
-			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'public/lib/angular-mocks/angular-mocks.js',
-			'public/modules/*/tests/*.js'
-		]
-	}
+  app: {
+    title: 'Codeaux',
+    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    keywords: 'Codeaux, Orbital'
+  },
+
+  port: process.env.PORT || 3000,
+  templateEngine: 'swig',
+  sessionSecret: 'MEAN',
+  sessionCollection: 'sessions',
+
+  assets: {
+    lib: {
+      // Using Angular Bootstrap instead of standard Bootstrap JavaScript.
+      css: [
+          'public/lib/bootstrap/dist/css/bootstrap.css',
+          'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+      ],
+      js: [
+          'public/lib/angular/angular.js',
+          'public/lib/angular-resource/angular-resource.js',
+          'public/lib/angular-cookies/angular-cookies.js',
+          'public/lib/angular-animate/angular-animate.js',
+          'public/lib/angular-touch/angular-touch.js',
+          'public/lib/angular-sanitize/angular-sanitize.js',
+          'public/lib/angular-ui-router/release/angular-ui-router.js',
+          'public/lib/angular-ui-utils/ui-utils.js',
+          'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+      ]
+    },
+    css: [
+        'public/modules/**/css/*.css'
+    ],
+    js: [
+        'public/config.js',
+        'public/application.js',
+        'public/modules/*/*.js',
+        'public/modules/*/*[!tests]*/*.js'
+    ],
+    tests: [
+        'public/lib/angular-mocks/angular-mocks.js',
+        'public/modules/*/tests/*.js'
+    ]
+  },
+
+  facebook: {
+    clientID: process.env.FACEBOOK_ID || 'APP_ID',
+    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    callbackURL: '/auth/facebook/callback'
+  },
+  google: {
+    clientID: process.env.GOOGLE_ID || 'APP_ID',
+    clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+    callbackURL: '/auth/google/callback'
+  },
+
+  github: {
+    clientID: process.env.GITHUB_ID || 'APP_ID',
+    clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+    callbackURL: '/auth/github/callback'
+  },
+
+  /*
+  linkedin: {
+    clientID: process.env.LINKEDIN_ID || 'APP_ID',
+    clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+    callbackURL: '/auth/linkedin/callback'
+  },
+  twitter: {
+    clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+    clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+    callbackURL: '/auth/twitter/callback'
+  },
+  */
+
+  mailer: {
+    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    options: {
+      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      auth: {
+        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+      }
+    }
+  },
 };
