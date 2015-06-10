146f880f516aaeadc7775cc17cda1deb49f5fa85
YanHua <yanhua_92@hotmail.com>
2015-06-10 19:08:37 +0800

    Update passport.js to ignore twitter and linkedin strategies.

--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,35 @@
 /**
  * Module dependencies.
  */
-var passport = require('passport'),
-	User = require('mongoose').model('User'),
-	path = require('path'),
-	config = require('./config');
+var passport = require('passport');
+var User = require('mongoose').model('User');
+var path = require('path');
+var config = require('./config');
 
 /**
  * Module init function.
  */
 module.exports = function() {
-	// Serialize sessions
-	passport.serializeUser(function(user, done) {
-		done(null, user.id);
-	});
+  // Serialize sessions
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
 
-	// Deserialize sessions
-	passport.deserializeUser(function(id, done) {
-		User.findOne({
-			_id: id
-		}, '-salt -password', function(err, user) {
-			done(err, user);
-		});
-	});
+  // Deserialize sessions
+  passport.deserializeUser(function(id, done) {
+    User.findOne({
+      _id: id
+    }, '-salt -password', function(err, user) {
+      done(err, user);
+    });
+  });
 
-	// Initialize strategies
-	config.getGlobbedFiles('./config/strategies/**/*.js').forEach(function(strategy) {
-		require(path.resolve(strategy))();
-	});
+  // Initialize strategies
+  config.getGlobbedFiles('./config/strategies/**/*.js').forEach(function(strategy) {
+    // Hack to ignore Twitter and Linkedin passport request.
+    // Remove this when both of their strategies JS file is remove from system.
+    if (~strategy.indexOf('linkedin.js') && ~strategy.indexOf('twitter.js')) {
+      require(path.resolve(strategy))();
+    }
+  });
 };
