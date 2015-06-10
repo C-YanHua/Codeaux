999f460de25f6a338607f10c3863953503a215e2
YanHua <yanhua_92@hotmail.com>
2015-06-10 19:32:53 +0800

    Update development port config.

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,11 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/codeaux-dev',
   app: {
     title: 'Codeaux - Development Environment',
     description: 'Codeaux development stage'
   }
+
+  db: 'mongodb://localhost/codeaux-dev',
+  port: process.env.PORT || 3000
 };
