962b902c4a5305d069ccc972890ad6fbefd5c4d2
YanHua <yanhua_92@hotmail.com>
2015-06-12 17:41:00 +0800

    Change to use HTML5mode for URL. Remove hashbang and hash prefix from app URL.

--- public/application.js
+++ public/application.js
@@ -1,13 +1,14 @@
 'use strict';
 
-//Start by defining the main module and adding the module dependencies
+// Start by defining the main module and adding the module dependencies.
 angular.module(ApplicationConfiguration.applicationModuleName,
                ApplicationConfiguration.applicationModuleVendorDependencies);
 
-// Setting HTML5 Location Mode
+// Setting HTML5 Location Mode.
 angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
   function($locationProvider) {
-    $locationProvider.hashPrefix('!');
+    // Enable app to use HTML5 History API.
+    $locationProvider.html5Mode(true);//.hashPrefix('!');
   }
 ]);
 
