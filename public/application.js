d034834e12d58b8e4628147e3ccacab1f68bc5d5
YanHua <yanhua_92@hotmail.com>
2015-06-13 14:12:10 +0800

    Change back to use hashbang so that project at least work for now.

--- public/application.js
+++ public/application.js
@@ -8,7 +8,7 @@ angular.module(ApplicationConfiguration.applicationModuleName,
 angular.module(ApplicationConfiguration.applicationModuleName).config(['$locationProvider',
   function($locationProvider) {
     // Enable app to use HTML5 History API.
-    $locationProvider.html5Mode(true);//.hashPrefix('!');
+    $locationProvider.hashPrefix('!');//.html5Mode(true);
   }
 ]);
 
