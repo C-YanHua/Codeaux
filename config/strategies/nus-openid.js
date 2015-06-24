9db50c701c858feeb35b22a4c90d789dd2d5363c
YanHua <yanhua_92@hotmail.com>
2015-06-24 18:15:07 +0800

    Add nusOpenId strategy.

---
+++ config/strategies/nus-openid.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Module dependencies.
+var passport = require('passport');
+var NUSStrategy = require('passport-nus-openid').Strategy;
+var config = require('../config');
+var users = require('../../app/controllers/users.server.controller');
+
+module.exports = function() {
+  // Use OpenID Strategy for NUS.
+  passport.use(new NUSStrategy({
+      returnURL: config.nusOpenId.returnURL,
+      realm: config.nusOpenId.realm,
+      profile: true,
+      passReqToCallback: true
+    },
+
+    function(req, identifier, profile, done) {
+      var providerUserProfile = {
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        displayName: profile.displayName,
+        email: profile.emails[0].value,
+        username: identifier.substring(identifier.lastIndexOf('/') + 1),
+        provider: 'nus',
+        providerIdentifierField: identifier
+      };
+
+      // Save the user OpenId profile.
+      users.saveOpenIdUserProfile(req, providerUserProfile, done);
+    }
+  ));
+};
