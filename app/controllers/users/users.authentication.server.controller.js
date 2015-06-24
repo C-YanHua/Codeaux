d14f39d49668d2309baee81cad26e4559d4215b0
YanHua <yanhua_92@hotmail.com>
2015-06-24 18:15:54 +0800

    Add OpenId authentication code.

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -1,35 +1,89 @@
 'use strict';
 
 // Module dependencies.
-var _ = require('lodash');
 var errorHandler = require('../errors.server.controller');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var User = mongoose.model('User');
 
-/**
- * Signup
- */
+// Private function for sign up new OAuth or OpenID user.
+var signupOAuthOrOpenId = function(searchQuery, possibleUsername, providerUserProfile, done) {
+  User.findOne(searchQuery, function(err, user) {
+    if (err) {
+      return done(err);
+    } else {
+      if (!user) {
+        User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
+          user = new User({
+            firstName: providerUserProfile.firstName,
+            lastName: providerUserProfile.lastName,
+            username: availableUsername,
+            displayName: providerUserProfile.displayName,
+            email: providerUserProfile.email,
+            provider: providerUserProfile.provider,
+            providerData: providerUserProfile.providerData
+          });
+
+          // And save the user
+          user.save(function(err) {
+            return done(err, user);
+          });
+        });
+      } else {
+        return done(err, user);
+      }
+    }
+  });
+};
+
+// Private function to merge provider data with existing logged in user.
+var mergeOAuthOrOpenIDProvider = function(req, providerUserProfile, done) {
+  // User is already logged in, join the provider data to the existing user
+  var user = req.user;
+
+  // Check if user exists, is not signed in using this provider,
+  // and doesn't have that provider data already configured.
+  if (user.provider !== providerUserProfile.provider &&
+      (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.provider])) {
+    // Add the provider data to the additional provider data field
+    if (!user.additionalProvidersData) {
+      user.additionalProvidersData = {};
+    }
+
+    user.additionalProvidersData[providerUserProfile.provider] = providerUserProfile.providerData;
+
+    // Then tell mongoose that we've updated the additionalProvidersData field
+    user.markModified('additionalProvidersData');
+
+    // And save the user
+    user.save(function(err) {
+      return done(err, user, 'settings/accounts');
+    });
+  } else {
+    return done(new Error('User is already connected using this provider'), user);
+  }
+};
+
+// New user sign up.
 exports.signup = function(req, res) {
-  // For security measurement we remove the roles from the req.body object.
+  // For security measurement, remove the roles from the req.body object.
   delete req.body.roles;
 
   // Init Variables.
   var user = new User(req.body);
-  var message = null;
 
-  // Add missing user fields
+  // Add missing user fields.
   user.provider = 'local';
   user.displayName = user.firstName + ' ' + user.lastName;
 
-  // Then save the user
+  // Save user into database.
   user.save(function(err) {
     if (err) {
       return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
+        message: err
       });
     } else {
-      // Remove sensitive data before login
+      // Remove sensitive data before login.
       user.password = undefined;
       user.salt = undefined;
 
@@ -44,15 +98,13 @@ exports.signup = function(req, res) {
   });
 };
 
-/**
- * Signin after passport authentication
- */
+// User sign in, either after passport authentication or local.
 exports.signin = function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err || !user) {
       res.status(400).send(info);
     } else {
-      // Remove sensitive data before login
+      // Remove sensitive data before login.
       user.password = undefined;
       user.salt = undefined;
 
@@ -67,26 +119,27 @@ exports.signin = function(req, res, next) {
   })(req, res, next);
 };
 
-/**
- * Signout
- */
+// User sign out.
 exports.signout = function(req, res) {
   req.logout();
   res.redirect('/');
 };
 
-/**
- * OAuth callback
- */
+// OpenID Return (Same implementation as OAuth Callback).
+exports.openIdReturn = function(strategy) {
+  return this.oauthCallback(strategy);
+};
+
+// OAuth callback.
 exports.oauthCallback = function(strategy) {
   return function(req, res, next) {
     passport.authenticate(strategy, function(err, user, redirectURL) {
       if (err || !user) {
-        return res.redirect('/signin');
+        return res.redirect('signin');
       }
       req.login(user, function(err) {
         if (err) {
-          return res.redirect('/signin');
+          return res.redirect('signin');
         }
 
         return res.redirect(redirectURL || '/');
@@ -95,86 +148,53 @@ exports.oauthCallback = function(strategy) {
   };
 };
 
-/**
- * Helper function to save or update a OAuth user profile
- */
+// Helper function to save or update a OpenID user profile.
+exports.saveOpenIdUserProfile = function(req, providerUserProfile, done) {
+  if (!req.user) {
+    // Define a search query to find existing user with current provider profile.
+    var searchQuery = {
+      provider: providerUserProfile.providerIdentifierField
+    };
+
+    var possibleUsername = providerUserProfile.username || providerUserProfile.email;
+
+    return signupOAuthOrOpenId(searchQuery, possibleUsername, providerUserProfile, done);
+  } else {
+
+    return mergeOAuthOrOpenIDProvider(req, providerUserProfile, done);
+  }
+};
+
+// Helper function to save or update a OAuth user profile.
 exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
   if (!req.user) {
-    // Define a search query fields
+    // Define a search query fields.
     var searchMainProviderIdentifierField = 'providerData.' + providerUserProfile.providerIdentifierField;
     var searchAdditionalProviderIdentifierField = 'additionalProvidersData.' + providerUserProfile.provider +
                                                   '.' + providerUserProfile.providerIdentifierField;
 
-    // Define main provider search query
+    // Define main provider search query.
     var mainProviderSearchQuery = {};
     mainProviderSearchQuery.provider = providerUserProfile.provider;
     mainProviderSearchQuery[searchMainProviderIdentifierField] =
       providerUserProfile.providerData[providerUserProfile.providerIdentifierField];
 
-    // Define additional provider search query
+    // Define additional provider search query.
     var additionalProviderSearchQuery = {};
     additionalProviderSearchQuery[searchAdditionalProviderIdentifierField] =
       providerUserProfile.providerData[providerUserProfile.providerIdentifierField];
 
-    // Define a search query to find existing user with current provider profile
+    // Define a search query to find existing user with current provider profile.
     var searchQuery = {
       $or: [mainProviderSearchQuery, additionalProviderSearchQuery]
     };
 
-    User.findOne(searchQuery, function(err, user) {
-      if (err) {
-        return done(err);
-      } else {
-        if (!user) {
-          var possibleUsername = providerUserProfile.username ||
-                                 ((providerUserProfile.email) ? providerUserProfile.email.split('@')[0] : '');
+    var possibleUsername = providerUserProfile.username || providerUserProfile.email;
 
-          User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
-            user = new User({
-              firstName: providerUserProfile.firstName,
-              lastName: providerUserProfile.lastName,
-              username: availableUsername,
-              displayName: providerUserProfile.displayName,
-              email: providerUserProfile.email,
-              provider: providerUserProfile.provider,
-              providerData: providerUserProfile.providerData
-            });
-
-            // And save the user
-            user.save(function(err) {
-              return done(err, user);
-            });
-          });
-        } else {
-          return done(err, user);
-        }
-      }
-    });
+    return signupOAuthOrOpenId(searchQuery, possibleUsername, providerUserProfile, done);
   } else {
-    // User is already logged in, join the provider data to the existing user
-    var user = req.user;
 
-    // Check if user exists, is not signed in using this provider,
-    // and doesn't have that provider data already configured.
-    if (user.provider !== providerUserProfile.provider &&
-        (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.provider])) {
-      // Add the provider data to the additional provider data field
-      if (!user.additionalProvidersData) {
-        user.additionalProvidersData = {};
-      }
-
-      user.additionalProvidersData[providerUserProfile.provider] = providerUserProfile.providerData;
-
-      // Then tell mongoose that we've updated the additionalProvidersData field
-      user.markModified('additionalProvidersData');
-
-      // And save the user
-      user.save(function(err) {
-        return done(err, user, '/settings/accounts');
-      });
-    } else {
-      return done(new Error('User is already connected using this provider'), user);
-    }
+    return mergeOAuthOrOpenIDProvider(req, providerUserProfile, done);
   }
 };
 
