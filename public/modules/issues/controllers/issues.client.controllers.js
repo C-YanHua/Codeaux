7437f906cf01ca62afc8d4372ebade108e2bcc71
WongKinFei <wongkinfei@gmail.com>
2015-07-12 02:28:11 +0800

    Search with pagination working for issues

--- public/modules/issues/controllers/issues.client.controllers.js
+++ public/modules/issues/controllers/issues.client.controllers.js
@@ -25,27 +25,46 @@ angular.module('issues').controller('IssuesCreateCtrl', ['$scope', '$stateParams
   }
 ]);
 
-angular.module('issues').controller('IssuesListCtrl', ['$scope', '$stateParams', '$location', 'Authentication', 'Issues',
-  function($scope, $stateParams, $location, Authentication, Issues) {
+angular.module('issues').controller('IssuesListCtrl', ['$scope', '$filter', '$stateParams', '$location', 'Authentication', 'Issues',
+  function($scope, $filter, $stateParams, $location, Authentication, Issues) {
     $scope.authentication = Authentication;
+
+    $scope.filteredIssues = [];
 
     // Find a list of Issues
     $scope.find = function() {
       $scope.issues = Issues.query(function() {
         $scope.currentPage = 1;
-        $scope.filteredIssues = [];
-        $scope.issuesPerPage = 2;
+        $scope.pagedIssues = [];
+        $scope.issuesPerPage = 5;
         $scope.numOfPageIcons = 5;
+
+        $scope.filteredIssues = $scope.issues.slice(0, $scope.issues.length);
       });
 
-      $scope.$watch('currentPage + issuesPerPage', function() {
+      $scope.$watch('currentPage + issuesPerPage + filteredIssues', function() {
         var begin = (($scope.currentPage - 1) * $scope.issuesPerPage);
         var end = begin + $scope.issuesPerPage;
 
-        $scope.first = begin;
-        $scope.second = end;
+        $scope.pagedIssues = $scope.filteredIssues.slice(begin, end);
+      });
+    };
 
-        $scope.filteredIssues = $scope.issues.slice(begin, end);
+    var searchMatch = function(stuff, target) {
+      if (!target) {
+        return true;
+      }
+      return stuff.toString().toLowerCase().indexOf(target.toLowerCase())!==-1;
+    };
+
+    $scope.search = function() {
+      $scope.filteredIssues = $filter('filter')($scope.issues, function(issue) {
+        for (var property in issue) {
+          if (searchMatch(issue[property], $scope.query)) {
+            return true;
+          }
+        }
+        return false;
       });
     };
   }
