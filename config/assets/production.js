7e0f39594919a415153b3d66dd975c2674462024
C-YanHua <yanhua_92@hotmail.com>
2015-07-30 14:13:21 +0800

    Fix angular upload file path invalid.

--- config/assets/production.js
+++ config/assets/production.js
@@ -18,7 +18,7 @@ module.exports = {
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-cookies/angular-cookies.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
