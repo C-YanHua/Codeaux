f94d00e9ef58d7cd861bd0564fc4131f655a4533
WongKinFei <wongkinfei@gmail.com>
2015-08-03 01:16:48 +0800

    Fixed bug for unable to find friends due to server routing problem

--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -9,11 +9,11 @@ module.exports = function(app) {
   app.route('/api/settings/profile').post(users.updateProfile);
   app.route('/api/settings/picture').post(users.changeProfilePicture);
 
-  // Setting up the Users Profile API.
-  app.route('/api/:username').get(users.read);
-
   // Search for users matching a search string.
   app.route('/api/users').get(users.search);
+
+  // Setting up the Users Profile API.
+  app.route('/api/:username').get(users.read);
 
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
   app.route('/api/users/password').post(users.changePassword);
